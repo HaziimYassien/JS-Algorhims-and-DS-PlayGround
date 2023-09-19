dd79f3fe1778f662d50d725fc4aadd699fc4a2d1
HaziimYassien <haziim.yassien@gmail.com>
2023-09-19 21:43:31 +0300

    stuff idk

--- Runner.js
+++ Runner.js
@@ -1,2 +1,20 @@
 //section 2 : The Big O Notation
 //6. Timing our code
+
+//section 4 : Problem Solving Approach
+//// 19. Introduction to Problem Solving
+// so we will take a set of steps to solve a problem
+// the steps
+//// first : understand the problem
+//1. understand the problem
+//explore concrete examples
+//break it down
+//solve/simplify
+//look back and refactor
+
+// Problem Solving Apporach:
+// 1. Understand the problem
+// 1.A Can I restate the problem in my own words
+// 1.B What are the inputs that go into the problem
+// 1.C What is the output? (what should be returned)
+// 1.D can the outputs be determined from the inputs?
